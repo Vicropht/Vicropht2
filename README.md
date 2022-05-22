a10d42e07bdc560c25037d707e18d3529ab32900
Vicropht <vicropht@protonmail.com>
2022-05-22 20:58:15 +0200

    Finish v1

---
+++ README.md
@@ -0,0 +1,6 @@
+    - For development:
+       ```yarn dev```
+    - For build:
+      ```yarn yarn build``` 
+    - For preview:
+      ```yarn serve```
--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -11,16 +11,19 @@ export class Container extends Component<Props, State> {
 
   render() {
     return (
-      <div className='wrapper'>
-        <h1 className='title'>Vicropht</h1>
+      <div className='container-center'>
+        <div className='wrapper'>
+          <h1 className='title'>Vicropht</h1>
+          <a href='https://discord.com/users/355435413705326597'>[root]#6722</a>
 
-        <Avatar />
+          <Avatar />
 
-        <div className='socials'>
-          <a href="https://twitter.com/CristophVictor">Twitter</a>
-          <a href="https://github.com/Vicropht" >GitHub</a>
-          <a href="mailto:vicropht@protonmail.com" >Contact</a>
-          <a href="https://www.linkedin.com/in/vicropht/" >LinkedIn</a>
+          <div className='socials'>
+            <a href="https://twitter.com/CristophVictor">Twitter</a>
+            <a href="https://github.com/Vicropht" >GitHub</a>
+            <a href="mailto:vicropht@protonmail.com" >Contact</a>
+            <a href="https://www.linkedin.com/in/vicropht/" >LinkedIn</a>
+          </div>
         </div>
       </div>
     )
