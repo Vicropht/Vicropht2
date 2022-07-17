56b98fdd34124a05f763934890881daaea8252ea
Vicropht <vicropht@protonmail.com>
2022-07-17 23:44:12 +0200

    create menu button templates

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { Modes } from '../../modes.enum';
 import WinIcon from '../../assets/win-logo.png';
 import CoffeeIcon from '../../assets/coffee.png';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 type Props = { changeMode: any };
 
@@ -20,20 +21,32 @@ export function Menu(props: Props) {
     props.changeMode(() => button.name as Modes);
   }
 
+  const menuHandler = () => {
+    setMenuOpen(open => !open);
+  }
+
+  const menuButtonFa = (icon: IconProp) => {
+    return (
+      <button id={`${menuOpen ? 'menuOpen' : ''}`} onClick={menuHandler}>
+        <FontAwesomeIcon icon={icon} />
+      </button>
+    )
+  }
+
+  const menuButtonSrc = (source: string) => {
+    return (
+      <button id={`${menuOpen ? 'menuOpen' : ''}`} onClick={menuHandler}>
+        <img src={source} width={30}></img>
+      </button>
+    )
+  }
+
   return (
     <div id='menuWrapper'>
-      <button id='burger'>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-      <button className='fadeInRight' name={`${Modes.default}`} title='Default mode' onClick={modeHandler}>
-        <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
-      </button>
-      <button className='fadeInRight' name={`${Modes.aestethic}`} title='A E S T E T H I C  M O D E' onClick={modeHandler}>
-        <img src={WinIcon} width={30}></img>
-      </button>
-      <button className='fadeInRight' name={`${Modes.comfy}`} title='comfy mode' onClick={modeHandler}>
-        <img src={CoffeeIcon} width={30}></img>
-      </button>
+      {menuButtonFa(faBars)}
+      {menuButtonFa(faHome)}
+      {menuButtonSrc(WinIcon)}
+      {menuButtonSrc(CoffeeIcon)}
     </div>
   );
 }
