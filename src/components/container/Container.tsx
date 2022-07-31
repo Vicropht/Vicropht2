ce636bd6ae9cfb01f52baffe59637ee5672e1c53
Vicropht <vicropht@protonmail.com>
2022-07-31 22:08:02 +0200

    add button for background noises iframe

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,9 +1,13 @@
-import React, { Component, ReactElement, useEffect, useState } from 'react'
-import './Container.scss'
-import Avatar from '../avatar/Avatar'
-import { Modes } from '../../modes.enum'
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactElement, useState } from 'react';
+import { Modes } from '../../modes.enum';
+import Avatar from '../avatar/Avatar';
+import './Container.scss';
 
 export function Container({ currentMode }: { currentMode: Modes }): ReactElement {
+  const [immcOpen, setImmcOpen] = useState(false);
+
   return (
     <div className='container-center'>
       <div className={`wrapper ${currentMode}`}>
@@ -15,7 +19,18 @@ export function Container({ currentMode }: { currentMode: Modes }): ReactElement
 
         {/* Display is set to none instead of creating a condition,
         this way the iframe doesn't have to load everytime the mode changes */}
-        <iframe src='https://imissmycafe.com/' id='coffe-soundboard' width={300} height={350}></iframe>
+        {
+          currentMode === Modes.comfy && (
+            <button onClick={() => setImmcOpen(open => !open)} className='noise'>
+              <FontAwesomeIcon icon={faMusic} /> Background noise
+            </button>
+          )
+        }
+        {
+          immcOpen && (
+            <iframe src='https://imissmycafe.com/' id='coffe-soundboard' width={300} height={350}></iframe>
+          )
+        }
 
         <div className='socials'>
           <a href="https://twitter.com/CristophVictor">Twitter</a>
